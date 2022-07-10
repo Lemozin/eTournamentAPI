3114fb7bbc0c698463a1c8d011d095c77874b03b
Leonard <leonard@neametrics.com>
2022-07-10 19:58:32 +0200

    Added auto generated unit test classes

---
+++ eTournamentAPITests/Controllers/TeamsControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using eTournamentAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTournamentAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class TeamsControllerTests
+    {
+        [TestMethod()]
+        public void TeamsControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteConfirmTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ eTournamentAPITests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using eTournamentAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTournamentAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class UsersControllerTests
+    {
+        [TestMethod()]
+        public void UsersControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteConfirmedTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
