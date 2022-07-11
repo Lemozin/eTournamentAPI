7931eb19361bb523310ef152fc45a0ee0949cfad
Leonard <leonard@neametrics.com>
2022-07-11 12:27:33 +0200

    Uniti testing modified

--- eTournamentAPITests/Controllers/MatchesControllerTests.cs
+++ eTournamentAPITests/Controllers/MatchesControllerTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using eTournamentAPI.Data.Enums;
+using eTournamentAPI.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace eTournamentAPI.Controllers.Tests;
 
@@ -39,5 +41,87 @@ public class MatchesControllerTests
     public void EditTest()
     {
         Assert.Fail();
+    }
+
+    public List<Match> GetAllMatches()
+    {
+        var allMatches = new List<Match>();
+        allMatches.AddRange(new List<Match>
+        {
+            new()
+            {
+                Name = "Champions League Final",
+                Description = "This is the Champions League Final description",
+                Price = 39.50,
+                ImageURL = "https://i.postimg.cc/pdbT2jX7/Match-1.jpg",
+                StartDate = DateTime.Now.AddDays(-10),
+                EndDate = DateTime.Now.AddDays(10),
+                TeamId = 3,
+                CoachId = 3,
+                MatchCategory = MatchCategory.Final
+            },
+            new()
+            {
+                Name = "Copa America Final",
+                Description = "This is the Copa America Final description",
+                Price = 29.50,
+                ImageURL = "https://i.postimg.cc/d31QWm47/Match-2.jpg",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(3),
+                TeamId = 1,
+                CoachId = 1,
+                MatchCategory = MatchCategory.Final
+            },
+            new()
+            {
+                Name = "Premier League",
+                Description = "This is the Premier League description",
+                Price = 39.50,
+                ImageURL = "https://i.postimg.cc/7L3LbL5L/Match-3.jpg",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(7),
+                TeamId = 4,
+                CoachId = 4,
+                MatchCategory = MatchCategory.Quarterfinals
+            },
+            new()
+            {
+                Name = "Europa League Final",
+                Description = "This is the Europa League Final description",
+                Price = 39.50,
+                ImageURL = "https://i.postimg.cc/W3s1ZPZw/Match-4.jpg",
+                StartDate = DateTime.Now.AddDays(-10),
+                EndDate = DateTime.Now.AddDays(-5),
+                TeamId = 1,
+                CoachId = 2,
+                MatchCategory = MatchCategory.Semifinals
+            },
+            new()
+            {
+                Name = "Euro 2022 Group Stage",
+                Description = "This is the Euro 2022 Group Stage description",
+                Price = 39.50,
+                ImageURL = "https://i.postimg.cc/rmgFCYCX/Match-5.jpg",
+                StartDate = DateTime.Now.AddDays(-10),
+                EndDate = DateTime.Now.AddDays(-2),
+                TeamId = 1,
+                CoachId = 3,
+                MatchCategory = MatchCategory.SecondRound
+            },
+            new()
+            {
+                Name = "Premier League - Fourth Round",
+                Description = "This is the Premier League - Fourth Round description",
+                Price = 39.50,
+                ImageURL = "https://i.postimg.cc/0y6fTTbn/Match-6.jpg",
+                StartDate = DateTime.Now.AddDays(3),
+                EndDate = DateTime.Now.AddDays(20),
+                TeamId = 1,
+                CoachId = 5,
+                MatchCategory = MatchCategory.FourthRound
+            }
+        });
+
+        return allMatches;
     }
 }
--- eTournamentAPITests/Controllers/TeamsControllerTests.cs
+++ eTournamentAPITests/Controllers/TeamsControllerTests.cs
@@ -1,43 +1,95 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using eTournamentAPI.Data.Services;
+using eTournamentAPI.Models;
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace eTournamentAPI.Controllers.Tests;
 
 [TestClass]
 public class TeamsControllerTests
 {
-    [TestMethod]
     public void TeamsControllerTest()
     {
         Assert.Fail();
     }
 
     [TestMethod]
-    public void IndexTest()
+    public async Task IndexTest()
     {
-        Assert.Fail();
+        // Arrange 
+        var fakeRecipes = A.CollectionOfDummy<Team>(GetAllTeams().Count).AsEnumerable();
+        var dataStore = A.Fake<ITeamService>();
+        A.CallTo(() => dataStore.GetAllAsync()).Returns(Task.FromResult(fakeRecipes));
+        var controller = new TeamsController(dataStore);
+
+        // Act
+        var actionResult = await controller.Index();
+
+        //Assert
+        var result = actionResult as OkObjectResult;
+        var returnRecipes = result.Value as IEnumerable<Team>;
+        Assert.Equals(GetAllTeams().Count, returnRecipes.Count());
     }
 
-    [TestMethod]
     public void CreateTest()
     {
         Assert.Fail();
     }
 
-    [TestMethod]
     public void DetailsTest()
     {
         Assert.Fail();
     }
 
-    [TestMethod]
     public void EditTest()
     {
         Assert.Fail();
     }
 
-    [TestMethod]
     public void DeleteConfirmTest()
     {
         Assert.Fail();
+    }
+
+    private List<Team> GetAllTeams()
+    {
+        var allTeams = new List<Team>();
+        allTeams.AddRange(new List<Team>
+        {
+            new()
+            {
+                Name = "Team 1",
+                Logo = "https://i.postimg.cc/BQnT6KNY/Team-1.jpg",
+                Description = "This is the description of the first team"
+            },
+            new()
+            {
+                Name = "Team 2",
+                Logo = "https://i.postimg.cc/VLK3rXwd/Team-2.jpg",
+                Description = "This is the description of the second team"
+            },
+            new()
+            {
+                Name = "Team 3",
+                Logo = "https://i.postimg.cc/Y9NPpW82/Team-3.jpg",
+                Description = "This is the description of the third team"
+            },
+            new()
+            {
+                Name = "Team 4",
+                Logo = "https://i.postimg.cc/GmhyL8JL/Team-4.jpg",
+                Description = "This is the description of the fourth team"
+            },
+            new()
+            {
+                Name = "Team 5",
+                Logo = "https://i.postimg.cc/XqSsmc57/Team-5.jpg",
+                Description = "This is the description of the fifth team"
+            }
+        });
+
+        return allTeams;
     }
 }
